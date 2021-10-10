b29edb6341e93e72e5a2796e818984aaa0438f32
rodrigo95 <rodrigo_06_27@hotmail.com>
2021-10-09 23:17:53 -0500

    agenda

---
+++ sesiones/sesion8/agenda/js/query.js
@@ -0,0 +1,13 @@
+
+function guardarDatos(){
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.movil = document.getElementById("movil").value;
+}
+
+function recuperarDatos(){
+    if ((localStorage.nombre != undefined) && (localStorage.movil != undefined)) {
+        document.getElementById("datos").innerHTML = "Nombre: "+localStorage.nombre +"</br>Movil: "+localStorage.movil
+    } else {
+        document.getElementById("datos").innerHTML = "No hay datos guardados"
+    }
+}
